de0513a9b404445dbac5c63151763b6b81046f7f
Piotr Machacki <piotr@machacki.pl>
2017-09-14 22:13:11 +0200

    Dodanie animowanych wejść elementów (wow). Dodanie popup lightbox (featherlight) dla menu.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,39 @@
-$('.carousel').carousel({
-	interval: 3000
+$(function() {
+
+	// CAROUSEL //
+	$('.carousel').carousel({
+		interval: 3000
+	});
+
+	// WOW ANIMATED //
+	wow = new WOW({
+		mobile: false
+	});
+	wow.init();
+
+	// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+	$('.navbar-collapse a').click(function() {
+		$(".navbar-collapse").collapse('hide');
+	});
+
+	// SMOOTH SCROLL //
+	$('.smoothScroll').on('click',function(e){
+		var targetId = $(this).attr('href');
+		if(targetId.indexOf('#') != -1) {
+			e.preventDefault();
+			split = targetId.split('#');
+			targetId = split[1];
+			if($('#'+targetId).length) {
+				$('html, body').animate({scrollTop: $('#'+targetId).offset().top}, 1000);
+			} else {
+				console.log('Element with ID "'+targetId+'" not exist.')
+			}
+		}
+	});
+
+	// FEATHERLIGHT //
+	$('.featherlight-link').featherlight({
+		targetAttr: 'href'
+	});
+
 });
